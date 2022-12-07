cbd91831a590cfb85ddc241342813a71600e0254
mtdung2 <mtdung2@cmcglobal.vn>
2022-12-07 18:18:26 +0700

    fix bug

--- src/main/java/com/example/wetravel/Repository/TourRepository.java
+++ src/main/java/com/example/wetravel/Repository/TourRepository.java
@@ -11,11 +11,11 @@ import java.util.List;
 
 @Repository
 public interface TourRepository extends JpaRepository<Tour , Long> {
-    @Query(value = "SELECT * FROM tour where (tour_name like :tourName or \"all\" like %:tourName%) and (tour_category_id in :tourCategoryList or -1 in :tourCategoryList)" +
+    @Query(value = "SELECT * FROM tour where (tour_name like :tourName or \"all\" like :tourName) and (tour_category_id in :tourCategoryList or -1 in :tourCategoryList)" +
             " and (tour_type = :tourType or -1 = :tourType) and (status = :status or -1 = :status) and ((adult_price between :fromPrice and :toPrice) or (-1 = :fromPrice and -1 = :toPrice))" , nativeQuery = true)
     Page<Tour> getListTour(String tourName , List<Integer> tourCategoryList , Integer tourType , Integer status ,Integer fromPrice, Integer toPrice , Pageable pageable);
 
-    @Query(value = "SELECT COUNT(*) FROM tour where (tour_name like :tourName or \"all\" like %:tourName%) and (tour_category_id in :tourCategoryList or -1 in :tourCategoryList)" +
+    @Query(value = "SELECT COUNT(*) FROM tour where (tour_name like :tourName or \"all\" like :tourName) and (tour_category_id in :tourCategoryList or -1 in :tourCategoryList)" +
             " and (tour_type = :tourType or -1 = :tourType) and (status = :status or -1 = :status) and ((adult_price between :fromPrice and :toPrice) or (-1 = :fromPrice and -1 = :toPrice))" , nativeQuery = true)
     Integer countListTour(String tourName , List<Integer> tourCategoryList , Integer tourType , Integer status ,Integer fromPrice ,Integer toPrice);
 }
