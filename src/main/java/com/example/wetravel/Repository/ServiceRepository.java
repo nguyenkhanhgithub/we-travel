2610b0fc32721e30ebd45cc92c95a0d986177fdc
mtdung2 <mtdung2@cmcglobal.vn>
2022-12-29 01:59:26 +0700

    fix bug

--- src/main/java/com/example/wetravel/Repository/ServiceRepository.java
+++ src/main/java/com/example/wetravel/Repository/ServiceRepository.java
@@ -19,12 +19,12 @@ public interface ServiceRepository extends JpaRepository<Service, Long> {
             "and (s.service_name like :serviceName or \"all\" like :serviceName)" +
             "and (s.service_category_id = :serviceCategoryId or 0 = :serviceCategoryId) and " +
             "(s.is_active = :isActive or -1 = :isActive) and (s.is_block = :isBlock or -1 = :isBlock)" +
-            " and (s.status = :status or -1 = :status) and (s.service_id in (:serviceIdList) or -1 in (:serviceIdList))" , nativeQuery = true)
+            " and (s.status = :status or -1 = :status) and (s.service_id in (:serviceIdList) or -1 in (:serviceIdList)) order by s.service_id desc " , nativeQuery = true)
     List<Service> getListServiceByCondition(Long accountId , String city , String serviceName , Long serviceCategoryId ,
                                             Integer isActive , Integer isBlock , Integer status , List<Long> serviceIdList);
 
     @Query(value = "select s from Service s join Partner p on s.partnerId.partnerId = p.partnerId join \n" +
-            "Account a on p.accountId.accountId = a.accountId where p.partnerId = :partnerId")
+            "Account a on p.accountId.accountId = a.accountId where p.partnerId = :partnerId order by s.serviceId desc ")
     Page<Service> getListServiceByPartnerId(Long partnerId , Pageable pageable);
 
     @Query(value = "SELECT s.* FROM service s join partner p on s.partner_id = p.partner_id where p.account_id = :accountId",nativeQuery = true)
